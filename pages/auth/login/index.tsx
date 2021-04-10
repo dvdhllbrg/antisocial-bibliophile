5e1f933d285c0a174e057d7116fa20457027de9b
David Hallberg Jönsson <d@vidhallberg.se>
2021-04-10 20:52:34 +0200

    fix

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -33,7 +33,7 @@ export default function Login() {
       </p>
       <button
         type="button"
-        className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded w-full sm:w-auto"
+        className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded w-full sm:w-auto"
         onClick={authenticateGoodreads}
       >
         Login with Goodreads
