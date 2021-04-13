28bdc491a1fb415a9966a59a7872a2a0e76bee5a
David Hallberg Jönsson <david.hallberg.jonsson@waya.se>
2021-04-13 09:35:12 +0200

    Paginated shelf

---
+++ components/BookList.tsx
@@ -0,0 +1,82 @@
+import useSWR from 'swr';
+import { Book } from '@custom-types/book';
+import formatDate from '@lib/formatDate';
+import formatNumber from '@lib/formatNumber';
+import BookCard from '@components/elements/BookCard';
+
+export const PAGE_SIZE = 10;
+
+type AuthorBookListProps = {
+  author: string;
+  index: number;
+  shelf?: never;
+  sort?: never;
+  sortOrder?: never;
+};
+
+type ShelfBookListProps = {
+  shelf: string;
+  index: number;
+  sort?: string;
+  sortOrder?: string;
+  author?: never;
+};
+
+type BookListProps = AuthorBookListProps | ShelfBookListProps;
+
+export default function BookList({
+  shelf, author, index, sort = 'date_added', sortOrder = 'd',
+}: BookListProps) {
+  const route = shelf
+    ? `/api/shelf?shelf=${shelf}&page=${index}&per_page=${PAGE_SIZE}&sort=${sort}&order=${sortOrder}`
+    : `/api/author/${author}/books?page=${index + 1}&per_page=${PAGE_SIZE}`;
+
+  const { data: books, error, isValidating } = useSWR<Book[]>(route);
+
+  const bookExtra = (book: Book) => {
+    switch (sort) {
+      case 'date_read':
+        return book.dateRead ? `Read ${formatDate(book.dateRead)}` : 'No read date.';
+      case 'date_updated':
+        return book.dateUpdated ? `Updated ${formatDate(book.dateUpdated)}` : 'No updated date.';
+      case 'date_added':
+        return book.dateAdded ? `added ${formatDate(book.dateAdded)}` : 'No added date.';
+      case 'year_pub':
+        return `First published in ${book.year || 'an unknown year'}`;
+      case 'avg_rating':
+        return `Goodreads rating: ${formatNumber(book.rating || 0)}`;
+      case 'rating':
+        return `My rating: ${formatNumber(book.myRating || 0)}`;
+      default:
+        return '';
+    }
+  };
+
+  if (error) {
+    return <div>failed to load</div>;
+  }
+  if (isValidating) {
+    return (
+      <>
+        <BookCard skeleton />
+        <BookCard skeleton />
+        <BookCard skeleton />
+        <BookCard skeleton />
+        <BookCard skeleton />
+        <BookCard skeleton />
+        <BookCard skeleton />
+      </>
+    );
+  }
+  return (
+    <>
+      {books?.map((book) => (
+        <BookCard
+          key={book.id}
+          book={book}
+          extra={bookExtra(book)}
+        />
+      ))}
+    </>
+  );
+}
--- pages/shelf/[name].tsx
+++ pages/shelf/[name].tsx
@@ -1,27 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import { useSWRInfinite } from 'swr';
 import { SortDescendingIcon } from '@heroicons/react/outline';
-import { Book } from '@custom-types/book';
-import formatDate from '@lib/formatDate';
-import formatNumber from '@lib/formatNumber';
 import isAuthed from '@lib/isAuthed';
 import useOnScreen from '@hooks/useOnScreen';
 import TopAppBar from '@components/TopAppBar';
-import BookCard from '@components/elements/BookCard';
-
-const PAGE_SIZE = 10;
-
-const getKey = (pageIndex: number, previousPageData: Book[] | null, params: any) => {
-  if (previousPageData && !previousPageData.length) {
-    return null;
-  }
-  const {
-    shelf, pageSize, sort, sortOrder,
-  } = params;
-  return `/api/shelf?shelf=${shelf}&page=${pageIndex + 1}&per_page=${pageSize}&sort=${sort}&order=${sortOrder}`;
-};
+import BookList, { PAGE_SIZE } from '@components/BookList';
 
 const SORT_OPTIONS = [
   { label: 'Date read', value: 'date_read' },
@@ -43,6 +27,7 @@ export default function Shelf() {
   const [sort, setSort] = useState('');
   const [sortOrder, setSortOrder] = useState('d');
   const [showSortMenu, setShowSortMenu] = useState(false);
+  const [page, setPage] = useState(1);
 
   if (!sort) {
     let initialSort = 'date_added';
@@ -55,72 +40,24 @@ export default function Shelf() {
     setSort(initialSort);
   }
 
-  const {
-    data: shelf, error, size, setSize, isValidating,
-  } = useSWRInfinite<Book[]>(
-    (...args) => getKey(...args, {
-      shelf: name,
-      pageSize: PAGE_SIZE,
-      sort,
-      sortOrder,
-    }),
-  );
-  const books = shelf ? ([] as Book[]).concat(...shelf) : [];
-  const isLoadingInitialData = !shelf && !error;
-  const isLoadingMore = (size > 0 && shelf && typeof shelf[size - 1] === 'undefined');
-  const isEmpty = shelf?.[0]?.length === 0;
-  const isReachingEnd = isEmpty || (shelf && shelf[shelf.length - 1]?.length < PAGE_SIZE);
-  const isRefreshing = isValidating && shelf && shelf.length === size;
-
   useEffect(() => {
-    if (loaderIsVisible && !isReachingEnd && !isRefreshing) {
-      setSize(size + 1);
+    if (loaderIsVisible) {
+      setPage(page + 1);
     }
-  }, [loaderIsVisible, isRefreshing]);
+  }, [loaderIsVisible]);
 
-  const bookExtra = (book: Book) => {
-    switch (sort) {
-      case 'date_read':
-        return book.dateRead ? `Read ${formatDate(book.dateRead)}` : 'No read date.';
-      case 'date_updated':
-        return book.dateUpdated ? `Updated ${formatDate(book.dateUpdated)}` : 'No updated date.';
-      case 'date_added':
-        return book.dateAdded ? `added ${formatDate(book.dateAdded)}` : 'No added date.';
-      case 'year_pub':
-        return `First published in ${book.year || 'an unknown year'}`;
-      case 'avg_rating':
-        return `Goodreads rating: ${formatNumber(book.rating || 0)}`;
-      case 'rating':
-        return `My rating: ${formatNumber(book.myRating || 0)}`;
-      default:
-        return '';
-    }
-  };
-
-  let content: {};
-  if (error) {
-    content = <div>failed to load</div>;
-  } else if (isLoadingInitialData) {
-    content = (
-      <>
-        <BookCard skeleton />
-        <BookCard skeleton />
-        <BookCard skeleton />
-        <BookCard skeleton />
-        <BookCard skeleton />
-        <BookCard skeleton />
-        <BookCard skeleton />
-      </>
+  const books = [];
+  for (let i = 0; i < page; i += 1) {
+    books.push(
+      <BookList
+        shelf={name as string}
+        index={page}
+        sort={sort}
+        sortOrder={sortOrder}
+      />,
     );
-  } else {
-    content = books.map((book) => (
-      <BookCard
-        key={book.id}
-        book={book}
-        extra={bookExtra(book)}
-      />
-    ));
   }
+
   return (
     <>
       <Head>
@@ -193,15 +130,8 @@ export default function Shelf() {
       </div>
       <main className={`container mx-auto p-4 transform-gpu transition-all duration-200 ease-out ${showSortMenu ? 'mt-0' : '-mt-24'}`}>
         <section className="max-w-screen-lg">
-          { content }
-          <div ref={loader}>
-            {isLoadingMore && (
-              <div className="flex flex-col justify-center items-center">
-                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900" />
-                Loading more...
-              </div>
-            )}
-          </div>
+          { books }
+          <div ref={loader} />
         </section>
       </main>
     </>
