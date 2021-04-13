e5ddb80b6888eafa63ad1b2707e65526703a240b
David Hallberg Jönsson <david.hallberg.jonsson@waya.se>
2021-04-13 14:44:03 +0200

    Test

--- pages/shelf/[name].tsx
+++ pages/shelf/[name].tsx
@@ -48,7 +48,7 @@ export default function Shelf() {
       <main className={`container mx-auto p-4 transform-gpu transition-all duration-200 ease-out ${showSortMenu ? 'mt-0' : '-mt-24'}`}>
         <section className="max-w-screen-lg">
           <BookList
-            baseRoute={`/api/shelf/${name}}`}
+            baseRoute={`/api/shelf/${name}`}
             perPage={PER_PAGE}
             initialSort={sort}
             showSort={showSortMenu}
