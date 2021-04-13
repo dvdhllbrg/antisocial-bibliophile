42475f38fc47e826eb2632ae204dfd0b329126b9
David Hallberg Jönsson <david.hallberg.jonsson@waya.se>
2021-04-13 10:16:38 +0200

    updates

---
+++ components/SortMenu.tsx
@@ -0,0 +1,76 @@
+type SortMenuProps = {
+  show: boolean;
+  sort: string;
+  setSort: (sort: string) => void;
+  sortOrder: string;
+  setSortOrder: (sort: string) => void;
+};
+
+const SORT_OPTIONS = [
+  { label: 'Date read', value: 'date_read' },
+  { label: 'Date updated', value: 'date_updated' },
+  { label: 'Date added', value: 'date_added' },
+  { label: 'Title', value: 'title' },
+  { label: 'Author', value: 'author' },
+  { label: 'First published', value: 'year_pub' },
+  { label: 'Goodreads rating', value: 'avg_rating' },
+  { label: 'My rating', value: 'rating' },
+];
+
+export default function SortMenu({
+  show, sort, setSort, sortOrder, setSortOrder,
+}: SortMenuProps) {
+  return (
+    <div className={`w-full bg-white p-4 transform-gpu transition-transform duration-200 ease-out ${show ? '' : '-translate-y-full'}`}>
+      <label
+        htmlFor="sort_by"
+        className="text-xs text-gray-600"
+      >
+        Sort by
+      </label>
+      <div className="flex items-center">
+        <select
+          id="sort_by"
+          className="bg-white flex-grow border-b border-gray-500 text-gray-800 p-2 pl-0 outline-none"
+          value={sort}
+          onChange={(e) => setSort(e.target.value)}
+        >
+          {SORT_OPTIONS.map(
+            (so) => (
+              <option
+                key={so.value}
+                value={so.value}
+              >
+                {so.label}
+              </option>
+            ),
+          )}
+        </select>
+        <div className="flex flex-col ml-4">
+          <label>
+            <input
+              type="radio"
+              name="sort_direction"
+              value="d"
+              checked={sortOrder === 'd'}
+              onChange={() => setSortOrder('d')}
+            />
+            {' '}
+            Descending
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sort_direction"
+              value="a"
+              checked={sortOrder === 'a'}
+              onChange={() => setSortOrder('a')}
+            />
+            {' '}
+            Ascending
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/shelf/[name].tsx
+++ pages/shelf/[name].tsx
@@ -6,17 +6,7 @@ import isAuthed from '@lib/isAuthed';
 import useOnScreen from '@hooks/useOnScreen';
 import TopAppBar from '@components/TopAppBar';
 import BookList, { PAGE_SIZE } from '@components/BookList';
-
-const SORT_OPTIONS = [
-  { label: 'Date read', value: 'date_read' },
-  { label: 'Date updated', value: 'date_updated' },
-  { label: 'Date added', value: 'date_added' },
-  { label: 'Title', value: 'title' },
-  { label: 'Author', value: 'author' },
-  { label: 'First published', value: 'year_pub' },
-  { label: 'Goodreads rating', value: 'avg_rating' },
-  { label: 'My rating', value: 'rating' },
-];
+import SortMenu from '@components/SortMenu';
 
 export default function Shelf() {
   const { query } = useRouter();
@@ -77,57 +67,13 @@ export default function Shelf() {
           <SortDescendingIcon className="h-6 w-6" />
         </button>
       </TopAppBar>
-      <div className={`w-full bg-white p-4 transform-gpu transition-transform duration-200 ease-out ${showSortMenu ? '' : '-translate-y-full'}`}>
-        <label
-          htmlFor="sort_by"
-          className="text-xs text-gray-600"
-        >
-          Sort by
-        </label>
-        <div className="flex items-center">
-          <select
-            id="sort_by"
-            className="bg-white flex-grow border-b border-gray-500 text-gray-800 p-2 pl-0 outline-none"
-            value={sort}
-            onChange={(e) => setSort(e.target.value)}
-          >
-            {SORT_OPTIONS.map(
-              (so) => (
-                <option
-                  key={so.value}
-                  value={so.value}
-                >
-                  {so.label}
-                </option>
-              ),
-            )}
-          </select>
-          <div className="flex flex-col ml-4">
-            <label>
-              <input
-                type="radio"
-                name="sort_direction"
-                value="d"
-                checked={sortOrder === 'd'}
-                onChange={() => setSortOrder('d')}
-              />
-              {' '}
-              Descending
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="sort_direction"
-                value="a"
-                checked={sortOrder === 'a'}
-                onChange={() => setSortOrder('a')}
-              />
-              {' '}
-              Ascending
-            </label>
-          </div>
-        </div>
-      </div>
+      <SortMenu
+        show={showSortMenu}
+        sort={sort}
+        setSort={setSort}
+        sortOrder={sortOrder}
+        setSortOrder={setSortOrder}
+      />
       <main className={`container mx-auto p-4 transform-gpu transition-all duration-200 ease-out ${showSortMenu ? 'mt-0' : '-mt-24'}`}>
         <section className="max-w-screen-lg">
           { books }
