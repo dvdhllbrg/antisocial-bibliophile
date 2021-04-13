396656abecef03321f9e2789166e986cfb9b81d7
David Hallberg Jönsson <david.hallberg.jonsson@waya.se>
2021-04-13 14:48:34 +0200

    Lo

--- hooks/useOnScreen.ts
+++ hooks/useOnScreen.ts
@@ -4,7 +4,7 @@ const useOnScreen = (ref: MutableRefObject<any>) => {
   const [isIntersecting, setIntersecting] = useState(false);
 
   useEffect(() => {
-    console.log(ref);
+    console.log(ref.current);
     const observer = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting));
     if (ref.current) {
       observer.observe(ref.current);
