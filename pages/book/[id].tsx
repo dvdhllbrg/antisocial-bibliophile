ab2815f2f0f99373851449910e402240d8891f55
David Hallberg Jönsson <d@vidhallberg.se>
2021-09-15 20:16:18 +0200

    Fix

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -18,15 +18,15 @@ import bookReducer from '@reducers/bookReducer';
 
 type BookPageProps = {
   id: string;
-  initialData: Book;
+  fallbackData: Book;
 };
 
-export default function BookPage({ id, initialData }: BookPageProps) {
+export default function BookPage({ id, fallbackData }: BookPageProps) {
   if (!id) {
     return null;
   }
 
-  const { book, isError: bookError } = useBook(id, initialData);
+  const { book, isError: bookError } = useBook(id, fallbackData);
   const { review, mutate } = useReview(id);
 
   const [shelfText, setShelfText] = useState('');
@@ -217,7 +217,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
       props: {
         id: params?.id,
-        initialData: bookReducer(book),
+        fallbackData: bookReducer(book),
       },
       revalidate: 1,
     };
