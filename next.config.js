7bdfcaf30d1f23e15351600846baef8a137a0642
David Hallberg Jönsson <d@vidhallberg.se>
2021-09-18 17:14:45 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -2,16 +2,33 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 const { createSecureHeaders } = require('next-secure-headers');
 
+const isProduction = process.env.node === 'production';
+
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
-    disable: process.env.NODE_ENV !== 'production',
+    disable: !isProduction,
   },
   images: {
     domains: ['images.gr-assets.com', 'i.gr-assets.com', 's.gr-assets.com'],
   },
   async headers() {
-    return [{ source: '/(.*)', headers: createSecureHeaders() }];
+    return [{
+      source: '/(.*)',
+      headers: createSecureHeaders({
+        contentSecurityPolicy: {
+          directives: {
+            baseURI: "'self'",
+            defaultSrc: "'self'",
+            scriptSrc: `'self' ${isProduction ? '' : 'unsafe-eval'}`,
+            styleSrc: "'self' 'unsafe-inline' data:",
+            imgSrc: "'self' https://*.gr-assets.com data: blob:",
+          },
+        },
+        forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+        referrerPolicy: 'same-origin',
+      }),
+    }];
   },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,23 +42,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="icons/apple-touch-icon.png"
+          href="/icons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="icons/favicon-32x32.png"
+          href="/icons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="icons/favicon-16x16.png"
+          href="/icons/favicon-16x16.png"
         />
         <link
           rel="mask-icon"
-          href="icons/safari-pinned-tab.svg"
+          href="/icons/safari-pinned-tab.svg"
           color="#5bbad5"
         />
         <meta
